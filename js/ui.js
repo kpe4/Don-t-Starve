c4131455646c5d31fbfd3ee5cfe48f8a9ad4671d
kpe4 <anafievtimur007@gmail.com>
2026-03-18 11:55:51 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,20 +1,6 @@
-function hello() {
+function helloUI() {
     console.log("UI ready");
 }
 
-// Функция для рисования полоски здоровья
-window.drawHealthBar = function(ctx, x, y, healthPercent) {
-    const barWidth = 200;
-    const barHeight = 20;
-    const fillWidth = (healthPercent / 100) * barWidth;
-    
-    // TODO: Нарисовать красный прямоугольник шириной fillWidth
-    // TODO: Нарисовать чёрную обводку вокруг всей полоски
-    ctx.fillStyle = 'red';
-    ctx.fillRect(x, y, fillWidth, barHeight);
-    
-    ctx.strokeStyle = 'black';
-    ctx.strokeRect(x, y, barWidth, barHeight);
-}
 
-hello()
+func helloUI()
